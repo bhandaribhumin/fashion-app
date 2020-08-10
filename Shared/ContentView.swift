c2457e41eb623eaca257ea2099ae6ff3d18beefa
Vitor Capretz <capretzvitor@gmail.com>
2020-08-10 23:20:42 +0200

    add images and buttons to onboarding

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,155 +12,52 @@ struct SlideContent {
     let title: String
     let subtitle: String
     let description: String
+    let imageName: String
 }
 
 enum ButtonVariant {
-    case primary
-    case standard
+    case primary, standard
 }
 
 let slides: [SlideContent] = [
-    SlideContent(color: Color(#colorLiteral(red: 0.7905165553, green: 0.932869494, blue: 0.969142735, alpha: 1)), title: "Relaxed", subtitle: "Find your outfits", description: "Confused about your outfit? Don't worry! Find the best outfit here!"),
-    SlideContent(color: Color(#colorLiteral(red: 0.7450980392, green: 0.9333333333, blue: 0.768627451, alpha: 1)), title: "Playful", subtitle: "Hear it first, wear it first", description: "Hating the clothes in your wardrobe? Explore hundreds of outfits ideas"),
-    SlideContent(color: Color(#colorLiteral(red: 1, green: 0.8941176471, blue: 0.8509803922, alpha: 1)), title: "Excentric", subtitle: "Your style, your way", description: "Create your individual & unique style and look amazing everyday"),
-    SlideContent(color: Color(#colorLiteral(red: 1, green: 0.8666666667, blue: 0.8666666667, alpha: 1)), title: "Funky", subtitle: "Look good, feel good", description: "Discover the latest trends in fashion and explore your personality"),
+    SlideContent(color: Color(#colorLiteral(red: 0.7905165553, green: 0.932869494, blue: 0.969142735, alpha: 1)), title: "Relaxed", subtitle: "Find your outfits", description: "Confused about your outfit? Don't worry! Find the best outfit here!", imageName: "onboarding1"),
+    SlideContent(color: Color(#colorLiteral(red: 0.7450980392, green: 0.9333333333, blue: 0.768627451, alpha: 1)), title: "Playful", subtitle: "Hear it first, wear it first", description: "Hating the clothes in your wardrobe? Explore hundreds of outfits ideas", imageName: "onboarding2"),
+    SlideContent(color: Color(#colorLiteral(red: 1, green: 0.8941176471, blue: 0.8509803922, alpha: 1)), title: "Excentric", subtitle: "Your style, your way", description: "Create your individual & unique style and look amazing everyday", imageName: "onboarding3"),
+    SlideContent(color: Color(#colorLiteral(red: 1, green: 0.8666666667, blue: 0.8666666667, alpha: 1)), title: "Funky", subtitle: "Look good, feel good", description: "Discover the latest trends in fashion and explore your personality", imageName: "onboarding4"),
 ]
+
+let slideCount: Int = slides.count - 1
 
 struct ContentView: View {
     @State private var selectedPage = 0
     @State private var selectedColor = slides[0].color
     
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                VStack(spacing: 0) {
-                    selectedColor
-                        .frame(width: geometry.size.width, height: geometry.size.height / 4 * 3)
-                        .animation(.easeInOut)
-                
-                    Path { path in
-                        path.move(to: CGPoint(x: 0, y: 50))
-                        
-                        path.addQuadCurve(
-                            to: .init(
-                                x: 50,
-                                y: 0
-                            ),
-                            control: .init(
-                                x: 0,
-                                y: 0
-                            )
-                        )
-                        
-                        path.addLine(
-                            to: .init(
-                                x: geometry.size.width - 50,
-                                y: 0
-                            )
-                        )
-                        
-                        path.addQuadCurve(
-                            to: .init(
-                                x: geometry.size.width,
-                                y: -50
-                            ),
-                            control: .init(
-                                x: geometry.size.width,
-                                y: 0
-                            )
-                        )
-                        
-                        path.addLine(
-                            to: .init(
-                                x: geometry.size.width,
-                                y: 50
-                            )
-                        )
-                        
-                        path.addLine(
-                            to: .init(
-                                x: 0,
-                                y: 50
-                            )
-                        )
-                    }
-                    .offset(y: -50)
-                    .fill(Color.white)
+        ZStack(alignment: .top) {
+            selectedColor
+                .clipShape(FunkyBorders())
+                .animation(.easeInOut)
+            
+            TabView(selection: $selectedPage) {
+                ForEach(0 ..< slides.count) { slideIndex in
+                    Slide(slide: slides[slideIndex], isTrailingText: slideIndex % 2 != 0, isLast: slideIndex == slideCount, slideIndex: slideIndex, selectedPage: $selectedPage)
+                        .tag(slideIndex)
                 }
-                
-                TabView(selection: $selectedPage) {
-                    ForEach(0 ..< slides.count) { slideIndex in
-                        Slide(title: slides[slideIndex].title, subtitle: slides[selectedPage].subtitle, description: slides[selectedPage].description, color: slides[slideIndex].color, isTrailingText: slideIndex % 2 != 0, variant: slideIndex == slides.count - 1 ? ButtonVariant.primary : ButtonVariant.standard)
-                            .tag(slideIndex)
-                    }
-                }
-                .onChange(of: selectedPage, perform: { value in
-                    selectedColor = slides[value].color
-                })
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             }
-            .edgesIgnoringSafeArea(.top)
+            .ignoresSafeArea()
+            .onChange(of: selectedPage, perform: { value in
+                selectedColor = slides[value].color
+            })
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .animation(.easeInOut)
+            
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct Slide: View {
-    var title: String
-    var subtitle: String
-    var description: String
-    var color: Color
-    var isTrailingText: Bool = false
-    var variant: ButtonVariant
-    
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack(alignment: .top) {
-                Text(title)
-                    .font(.system(size: 80))
-                    .foregroundColor(.white)
-                    .bold()
-                    .rotationEffect(.degrees((isTrailingText ? -1 : 1) * 90))
-                    .offset(y: geometry.size.height / 6)
-                    .offset(x: ((isTrailingText ? 1 : -1) * geometry.size.width / 2) + ((isTrailingText ? -1 : 1) * 50))
-            
-                VStack(spacing: 12) {
-                    Spacer()
-                    
-                    Text(subtitle)
-                        .fontWeight(.semibold)
-                        .font(.title)
-                        .foregroundColor(Color(#colorLiteral(red: 0.04705882353, green: 0.05098039216, blue: 0.2039215686, alpha: 1)))
-                    
-                    
-                    Text(description)
-                        .font(.callout)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color(#colorLiteral(red: 0.04705882353, green: 0.05098039216, blue: 0.2039215686, alpha: 1)))
-                        .padding(.bottom, 40)
-                    
-                    Button(action: {
-                        
-                    }) {
-                        Text("Next")
-                            .foregroundColor(.black)
-                    }
-                    .padding(.horizontal, 100)
-                    .padding(.vertical, 15)
-                    .background(variant == .primary ? Color.black : Color.grey)
-                    .clipShape(RoundedRectangle(cornerRadius: 25))
-                }
-                .padding(.horizontal, 44)
-                .frame(height: geometry.size.height)
-                .offset(y: -geometry.size.height / 8)
-                
-            }
-            .frame(maxWidth: .infinity)
-        }
     }
 }
---
+++ Shared/Slide.swift
@@ -0,0 +1,150 @@
+//
+//  Slide.swift
+//  Fashion
+//
+//  Created by Vitor Capretz on 2020-08-09.
+//
+
+import SwiftUI
+
+struct Slide: View {
+    var slide: SlideContent
+    var isTrailingText: Bool = false
+    var isLast: Bool
+    var slideIndex: Int
+    @Binding var selectedPage: Int
+    
+    var variant: ButtonVariant {
+        if isLast {
+            return .primary
+        }
+        
+        return .standard
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .top) {
+                Image(slide.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .clipShape(FunkyBorders())
+                    .offset(y: -48)
+                
+                Text(slide.title)
+                    .font(.system(size: 80))
+                    .foregroundColor(.white)
+                    .bold()
+                    .rotationEffect(.degrees((isTrailingText ? -1 : 1) * 90))
+                    .offset(y: geometry.size.height / 6)
+                    .offset(x: ((isTrailingText ? 1 : -1) * geometry.size.width / 2) + ((isTrailingText ? -1 : 1) * 50))
+            
+                VStack(spacing: 12) {
+                    Spacer()
+                    
+                    Text(slide.subtitle)
+                        .fontWeight(.semibold)
+                        .font(.title)
+                        .foregroundColor(Color(#colorLiteral(red: 0.04705882353, green: 0.05098039216, blue: 0.2039215686, alpha: 1)))
+                    
+                    
+                    Text(slide.description)
+                        .font(.callout)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(#colorLiteral(red: 0.04705882353, green: 0.05098039216, blue: 0.2039215686, alpha: 1)))
+                        .padding(.bottom, 40)
+                    
+                    Button(action: navigate) {
+                        Text(variant == .primary ? "Let's get started" : "Next")
+                            .foregroundColor(variant == .primary ? .white : Color(#colorLiteral(red: 0.04705882353, green: 0.05098039216, blue: 0.2039215686, alpha: 1)))
+                            .frame(width: 245, height: 50)
+                            .background(Color(variant == .primary ? #colorLiteral(red: 0.1725490196, green: 0.7254901961, blue: 0.6901960784, alpha: 1) : #colorLiteral(red: 0.04705882353, green: 0.05098039216, blue: 0.2039215686, alpha: 0.05121630933)))
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                    }
+                }
+                .padding(.horizontal, 44)
+                .frame(height: geometry.size.height)
+                .offset(y: -geometry.size.height / 8)
+                
+            }
+        }
+    }
+    
+    func navigate() {
+        if !isLast {
+            selectedPage = slideIndex + 1
+        }
+    }
+}
+
+struct FunkyBorders: Shape {
+    let curveSize = 50
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let lineHeight = Int(rect.height * 0.66)
+        
+        path.move(to: CGPoint(x: 0, y: lineHeight + curveSize))
+        
+        path.addQuadCurve(
+            to: .init(
+                x: curveSize,
+                y: lineHeight
+            ),
+            control: .init(
+                x: 0,
+                y: lineHeight
+            )
+        )
+
+        path.addLine(
+            to: .init(
+                x: Int(rect.width) - curveSize,
+                y: lineHeight
+            )
+        )
+
+        path.addQuadCurve(
+            to: .init(
+                x: rect.width,
+                y: CGFloat(lineHeight - curveSize)
+            ),
+            control: .init(
+                x: Int(rect.width),
+                y: lineHeight
+            )
+        )
+        
+        path.addLine(
+            to: .init(
+                x: Int(rect.width),
+                y: Int(rect.minY)
+            )
+        )
+
+        path.addLine(
+            to: .init(
+                x: Int(rect.minX),
+                y: Int(rect.minY)
+            )
+        )
+
+        return path
+    }
+}
+
+struct Slide_Previews: PreviewProvider {
+    static let slideTest: SlideContent = SlideContent(color: .pink, title: "Test", subtitle: "Some more title", description: "A longer description here would fit super nicely on the screen", imageName: "onboarding1")
+    
+    static var previews: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.7905165553, green: 0.932869494, blue: 0.969142735, alpha: 1))
+                .clipShape(FunkyBorders())
+            
+            Slide(slide: slideTest, isTrailingText: false, isLast: true, slideIndex: 0, selectedPage: .constant(1))
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
